790665aec70f50f49e663e8874352a48da9c60ed
jovi <kwong.jovi@gmail.com>
2022-12-04 08:53:37 +0000

    test(findOdd): add all example tests from instructions

--- javascript/find-odd.test.js
+++ javascript/find-odd.test.js
@@ -1,5 +1,24 @@
 const findOdd = require("./find-odd");
 
-describe("findOdd: returns the integer that appears an odd number of times in a given array of integers.", () => {
-  it("given ", () => {});
+describe("findOdd", () => {
+  it("given [7], return 7", () => {
+    const input = [7];
+    expect(findOdd(input)).toBe(7);
+  });
+  it("given [0], return 0", () => {
+    const input = [0];
+    expect(findOdd(input)).toBe(0);
+  });
+  it("given [1, 1, 2], return 2", () => {
+    const input = [1, 1, 2];
+    expect(findOdd(input)).toBe(2);
+  });
+  it("given [0, 1, 0, 1, 0], return 0", () => {
+    const input = [0, 1, 0, 1, 0];
+    expect(findOdd(input)).toBe(0);
+  });
+  it("given [1,2,2,3,3,3,4,3,3,3,2,2,1], return 4", () => {
+    const input = [1, 2, 2, 3, 3, 3, 4, 3, 3, 3, 2, 2, 1];
+    expect(findOdd(input)).toBe(4);
+  });
 });
