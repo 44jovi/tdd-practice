f5bda6238744edafdeea9c17034a9efe25e652ff
jovi <kwong.jovi@gmail.com>
2022-12-04 09:12:40 +0000

    test(findOdd): implement function logic. Pass all tests

--- javascript/find-odd.js
+++ javascript/find-odd.js
@@ -1,6 +1,15 @@
 function findOdd(A) {
-  //happy coding!
-  return 0;
+  let counter = {};
+
+  A.forEach((n) => {
+    counter[n] = (counter[n] || null) + 1;
+  });
+
+  for (key in counter) {
+    if (counter[key] % 2 != 0) {
+      return parseInt(key);
+    }
+  }
 }
 
 module.exports = findOdd;
