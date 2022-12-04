c446d1c3b32a7d90c935ca4f8837dbca3b2850fb
jovi <kwong.jovi@gmail.com>
2022-12-04 08:42:28 +0000

    test(findOdd): set up test suite

---
+++ javascript/find-one.test.js
@@ -0,0 +1,5 @@
+const findOdd = require("./find-one");
+
+describe("findOdd: returns the integer that appears an odd number of times in a given array of integers.", () => {
+  it("", () => {});
+});
