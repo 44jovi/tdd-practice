97d94542eb98c8a2c3cb20833dc257d02b53ab38
jovi <kwong.jovi@gmail.com>
2022-12-06 19:49:55 +0000

    test(digitalRoot): pass test for input of 942

--- javascript/digital-root/digital-root.test.js
+++ javascript/digital-root/digital-root.test.js
@@ -9,7 +9,7 @@ describe("digitalRoot - returns the digital root of a given integer", () => {
     expect(digitalRoot(16)).toBe(7);
   });
 
-  xit("Input: 942", () => {
+  it("Input: 942", () => {
     expect(digitalRoot(942)).toBe(6);
   });
 });
