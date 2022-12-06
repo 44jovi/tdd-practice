0154b22620edc7e280e45436f434b21a67d9bb11
jovi <kwong.jovi@gmail.com>
2022-12-06 19:51:39 +0000

    test(digitalRoot): pass test for input of 493193

--- javascript/digital-root/digital-root.test.js
+++ javascript/digital-root/digital-root.test.js
@@ -16,4 +16,8 @@ describe("digitalRoot - returns the digital root of a given integer", () => {
   it("Input: 132189", () => {
     expect(digitalRoot(132189)).toBe(6);
   });
+
+  it("Input: 493193", () => {
+    expect(digitalRoot(493193)).toBe(2);
+  });
 });
