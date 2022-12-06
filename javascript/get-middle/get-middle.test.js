3305dcfb720243bd3fad8e070b4e75e0760531db
jovi <kwong.jovi@gmail.com>
2022-12-06 10:31:59 +0000

    test(getMiddle): pass test with a single character. Reword previous tests

--- javascript/get-middle/get-middle.test.js
+++ javascript/get-middle/get-middle.test.js
@@ -1,15 +1,19 @@
 const getMiddle = require("./get-middle");
 
 describe("getMiddle - returns middle character(s) of a word", () => {
-  it("Input: 'test'", () => {
+  it("Input: even character length (4)", () => {
     expect(getMiddle("test")).toBe("es");
   });
 
-  it("Input: 'testing'", () => {
+  it("Input: odd character length (7)", () => {
     expect(getMiddle("testing")).toBe("t");
   });
 
-  it("Input: 'middle'", () => {
+  it("Input: even character length (6)", () => {
     expect(getMiddle("middle")).toBe("dd");
+  });
+
+  it("Input: single character'", () => {
+    expect(getMiddle("A")).toBe("A");
   });
 });
